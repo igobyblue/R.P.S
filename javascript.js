3ed76c064d42287dd6d64ce0257d1eef79b93051
igobyblue <noahhernandez960@gmail.com>
2023-07-13 16:36:14 -0700

    foundation for my rock paper scissors game.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const choices = ["rock", "paper", "scissors"]
+
+function game() {
+    //plays the game.
+    //plays five rounds
+    //console based
+}
+
+function playRound() {
+    const playerSelection =
+}
+
+function playerChoice () {
+    //input from player goes here
+}
+
+function computerChoice() {
+    //random input from computer here
+}
