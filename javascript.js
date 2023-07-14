bbf7f03e6f1f687c378a9b5d086436a3298455e2
igobyblue <noahhernandez960@gmail.com>
2023-07-13 17:13:39 -0700

    Able to type in a user inputed prompt, fixing a loop incase of user error in spelling.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,35 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"] //game picks between these inputs
 
-function game() {
-    //plays the game.
-    //plays five rounds
-    //console based
+function game() { //this starts the game
+    playRound();
 }
 
-function playRound() {
-    const playerSelection =
+function playRound() { // this lets the players choice go against the computers
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
 }
 
-function playerChoice () {
-    //input from player goes here
+function playerChoice() {
+    let input = prompt('Type Rock, Paper, or Scissors');
+    while(input == null){
+        input = prompt('Type Rock, Paper, or Scissors'); // allows canceled or blank input to not break code
+    }
+    input = input.toLowerCase(); //allows this to make player sting to be all lower case
+    let check = validateInput(input); // checks if input is correct
+   while (check == false) {
+    input = prompt('Error: Please make sure you type: Rock, Paper or Scissors');
+    input = input.toLowerCase();
+    check = validateInput(input);   
+    }
 }
 
 function computerChoice() {
-    //random input from computer here
+    return choices[Math.floor(Math.random()*choices.length)]
 }
+
+
+function validateInput(choice){
+   return choices.includes(choice);
+}
+
+game();
