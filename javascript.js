68a442a3f0d7313ca69851541d427bdbb8284ea4
igobyblue <noahhernandez960@gmail.com>
2023-08-20 15:53:46 -0700

    setting up UI and interactive images to play on.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,25 @@
 const choices = ["rock", "paper", "scissors"] //game picks between these inputs
 const winners = [];
 
-function game() { //this starts the game
-    for (let i = 1; i <= 5; i++) {
-        playRound(i);
-    }
-    logWins();
-}
+function resetGame();
 
-function playRound(round) { // this lets the players choice go against the computers
-    const playerSelection = playerChoice();
+function startGame(); { //this starts the game
+    let imgs = document.querySelector('img')
+    imgs.forEach((img) =>
+    img.addEventListener(('click'), () => {
+        if (img.id) {
+            playRound(img.id);
+        }
+    })
+    )
+};
+
+function playRound(playerChoice); { // this lets the players choice go against the computers
     const computerSelection = computerChoice();
+
     const winner = checkWinner(playerSelection,computerSelection);
+
     winners.push(winner);
-    logRound(playerSelection,computerSelection,winner,round)
 }
 
 function playerChoice() {
@@ -45,6 +51,7 @@ function validateInput(choice){ // checks to see if input is one of those three 
    return choices.includes(choice);
 }
 
+
 function checkWinner(choiceP, choiceC){ // tells computer what choice wins over what
     if (choiceP === choiceC) {
         return 'Tie';
@@ -63,16 +70,9 @@ function logWins() { // tracks the wins of player and computer
     let playerWins = winners.filter((bit) => bit == "Player").length;
     let computerWins = winners.filter((bit) => bit == "Computer").length;
     let ties = winners.filter((bit) => bit == "Tie").length;
-    console.log("Results:");
-    console.log("Player Wins:" , playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:" , ties);
+
 }
 
 function logRound(playerChoice,computerChoice,winner,round) { //
-console.log ("Round:", round);
-console.log('Player Chose:',playerChoice);
-console.log('Computer Chose:',computerChoice);
-console.log(winner, "Won the Round");
-console.log("----------------------------------------------------")
+
 }
